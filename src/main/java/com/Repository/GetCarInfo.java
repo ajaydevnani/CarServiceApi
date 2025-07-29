efc55e33c318d016bf6f4a5c5b48e5559d3c44de
Ajay Kumar <ajaydevnani123@gmail.com>
2025-07-28 22:17:17 -0500

    make it call jdbc template instead of DatabaseClient(used for r2dbc)

--- src/main/java/com/Repository/GetCarInfo.java
+++ src/main/java/com/Repository/GetCarInfo.java
@@ -1,30 +1,32 @@
 package com.Repository;
 
-
 import com.config.SqlQueriesConfig;
-import com.models.car;
+import com.models.Car;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.r2dbc.core.DatabaseClient;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-@Slf4j
 @RequiredArgsConstructor
+@Slf4j
 public class GetCarInfo {
 
-    private DatabaseClient databaseClient;
 
-    @Autowired
-    SqlQueriesConfig sqlQueriesConfig;
+    private final JdbcTemplate jdbcTemplate;
+    private final SqlQueriesConfig sqlQueriesConfig;
 
-    public List<car> getCarInfo() {
-        String query = sqlQueriesConfig.getCarInfo;
-        return databaseClient.sql(query)
-                .mapProperties(car.class).all().collectList().block();
+    public List<Car> getCarInfo() {
+        String query = sqlQueriesConfig.getCarInfo();
+        return jdbcTemplate.query(query, (result, index) ->
+                Car.builder()
+                        .make(result.getString("make"))
+                        .model(result.getString("model"))
+                        .year(result.getInt("year"))
+                        .trim(result.getString("trim"))
+                        .build()
+        );
     }
-
 }
