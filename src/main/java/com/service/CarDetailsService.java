ca0b85c61996f46f50f3b5e93c4359507e26e3a3
Ajay Kumar <ajaydevnani123@gmail.com>
2025-07-28 22:21:30 -0500

    remove autowired and added ReqArgsconstructor.

--- src/main/java/com/service/CarDetailsService.java
+++ src/main/java/com/service/CarDetailsService.java
@@ -1,20 +1,19 @@
 package com.service;
 
 import com.Repository.GetCarInfo;
-import com.models.car;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.models.Car;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CarDetailsService {
 
-    @Autowired
-    GetCarInfo getCarInfo;
+    private final GetCarInfo getCarInfo;
 
-    public List<car> getCarDetails() {
+    public List<Car> getCarDetails() {
         return getCarInfo.getCarInfo();
     }
 }
