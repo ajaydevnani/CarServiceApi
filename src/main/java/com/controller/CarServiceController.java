175afadf293f68db26e1832ed53c9b78b8ea5a31
Ajay Kumar <ajaydevnani123@gmail.com>
2025-07-28 22:17:56 -0500

    remove autowired and added RequiredArgsConstructor

--- src/main/java/com/controller/CarServiceController.java
+++ src/main/java/com/controller/CarServiceController.java
@@ -1,26 +1,25 @@
 package com.controller;
 
-import com.models.car;
+import com.models.Car;
 import com.service.CarDetailsService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @Slf4j
 @RestController
 @RequestMapping("/cars")
+@RequiredArgsConstructor
 public class CarServiceController {
 
-    @Autowired
-    CarDetailsService carDetailsService;
+    private final CarDetailsService carDetailsService;
 
     @GetMapping
-    public List<car> getAllCars() {
+    public List<Car> getAllCars() {
         return carDetailsService.getCarDetails();
     }
 }
